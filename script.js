dc4299be853dbb68da32339fadda71ca2cb59cbd
Roman-Daher <84515748+RomanBallinFaxJah@users.noreply.github.com>
2021-06-10 16:23:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+myage = 15
+let guess
+
+while (guess !== myage) {
+  guess = parseInt(prompt('Guess my age'))
+  if (guess === myage) {
+    alert('You won!')
+  } else if (guess > myage) {
+    alert('Too high!')
+  } else {
+    alert('Too low!')
+  }
+}
